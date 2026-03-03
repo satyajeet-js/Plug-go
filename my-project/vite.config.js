d9853e8908892fb0aa4e6f4f1a5df9c6f94d9e38
satyajeet-js <satyajeet2004patil@gmail.com>
2026-03-03 16:18:44 +0530

    Update vite.config.js

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  
+  build: {
+    chunkSizeWarningLimit: 1000, // 👈 Increase limit (default 500kb)
+
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          reactVendor: ["react", "react-dom"],
+          router: ["react-router-dom"],
+          axios: ["axios"]
+        }
+      }
+    }
+  }
+});
